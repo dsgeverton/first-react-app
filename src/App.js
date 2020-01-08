4d4859757fdc3a4c87e992055b9e1cf746ee1667
EVERTON DA SILVA GONCALVES <CORP\evertongoncalves@no018003.corp.campos.rj.gov.br>
2020-01-08 10:17:41 -0300

    modifying to dynamic inputs

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './App.css';
 import Conversor from './conversor/Conversor'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Conversor moedaA="USD" moedaB="BRL"></Conversor>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+    </div>
+  );
 }
 
 export default App;
